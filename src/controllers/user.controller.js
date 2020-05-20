5ec4e6d38bba8d14ba6e0469e589ce721cdcd404
Jairo Hernandez <jairohernandezl@gmail.com>
2020-05-20 13:17:56 +0000

    Se agregan validaciones basicas

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,8 +7,12 @@ module.exports = {
         res.status(200).json(users);
     },
     async create(req, res){
+        try{ 
         const user = await User.create({...req.body});
         res.status(200).json(user);
+        } catch (error){
+            res.status(401).json(error);
+        }  
     }, 
     async update (req, res){
         const options ={
