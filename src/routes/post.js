388b98a5a9575dbbffc87e33d4ac61a7df6486ca
Jairo Hernandez <jairohernandezl@gmail.com>
2020-05-29 22:19:09 +0000

    Se agrega archivo README, se modificael list de CRUD de posts para que solo devuelva los posts de un proveedor en especifico.

--- src/routes/post.js
+++ src/routes/post.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const postController = require('../controllers/post.controller');
 const {auth, verify} = require('../utils/middleware');
 
-router.route('/').get(postController.list);
+router.route('/').get(auth, postController.list);
 router.route('/').post(auth, postController.create); 
 router.route('/:postId').put(verify, postController.update);  //Actualizar un post
 router.route('/:postId').get(postController.show);
