22e6b8566982d182207e37a79cdd5be3ef07efb7
Ubuntu <jhhernan@TopV1.b3mtr5aldv3e3h5tbxf2pgyqqe.bx.internal.cloudapp.net>
2020-06-18 23:14:53 +0000

    Se crean las pruebas del server

--- src/routes/post.test.js
+++ src/routes/post.test.js
@@ -13,12 +13,13 @@ describe('post', () => {
         }
     });
 
-    beforeAll(async () => {
+    beforeAll(async done => {
         const user = {email:"jairo1@test.com", 
         password: "12345",
         username:"jhhernan", name:"Jairo", lastname:"Hernandez"};
         const auth = await req(app).post("/users/signup").send(user);
         token = auth.body;
+        done();
     })
 
     afterAll(async () => {
@@ -29,27 +30,27 @@ describe('post', () => {
 
         const query = await req(app).get("/posts").set("Authorization",token);
 
-        done();
         expect(query.statusCode).toBe(200);
         expect(query.body).toHaveLength(0);
+        done();
     });
     
     it('should display an error message if a bad token is used', async done => {
         const token = "badtoken";
         const query = await req(app).get("/posts").set("Authorization",token);
         
-        done();
         expect(query.statusCode).toBe(401);
         expect(query.body).toHaveProperty("message", "jwt malformed");
+        done();
     });
 
     it('should display an error message if a expired token is used', async done => {
         const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlZWE1MmJjYjg4ZTI1M2E1YjY3MmJlNiIsImlhdCI6MTU5MjQxNDkwOCwiZXhwIjoxNTkyNDE4NTA4fQ.yx2XnYj2VWpxmacrN61ksR2nJiV_WwfkVJK1vBJr_zc";
         const query = await req(app).get("/posts").set("Authorization",token);
         
-        done();
         expect(query.statusCode).toBe(401);
         expect(query.body).toHaveProperty("message", "jwt expired");
+        done();
     });
 
     it('should create a new post', async done => {
@@ -58,12 +59,23 @@ describe('post', () => {
         .field('title', "New Title")
         .field('description', "New description")
         .attach('photo', 'public/uploads/images/test.jpg');
-        //.attach('file', buffer, 'filename')
-        done();
-                    //Ojo, en el uth no estamos validando que el suuario efectivamente exista. OJO
+
         expect(res.statusCode).toBe(200);
         expect(res.body).toHaveProperty("title", "New Title");
+        done();
     });
+
+    it('shouldnt create a new post if title is missing', async done => {
+        
+        const res = await req(app).post("/posts").set("Authorization",token)
+        .field('description', "New description")
+        .attach('photo', 'public/uploads/images/test.jpg');
+    
+        expect(res.statusCode).toBe(401);
+        expect(res.body).toHaveProperty("message", "Post validation failed: title: A title is required");
+        done();
+    });
+
 
     it('should update the title of an existing post', async done => {
         //Se podria crear con mongoose directamente, pero preferimos con 
@@ -77,10 +89,25 @@ describe('post', () => {
         const update = await req(app).put("/posts/"+post_id).set("Authorization",token)
         .field('title', "Updated Title");
 
-        done();
-                    //Ojo, en el auth no estamos validando que el suuario efectivamente exista. OJO
+        //Ojo, en el auth no estamos validando que el suuario efectivamente exista. OJO
         expect(update.statusCode).toBe(200);
         expect(update.body).toHaveProperty("title", "Updated Title");
+        done();
+    });
+
+    it('should update the image of an existing post', async done => {
+        const res = await req(app).post("/posts").set("Authorization",token)
+        .field('title', "New Title")
+        .field('description', "New description")
+        .attach('photo', 'public/uploads/images/test.jpg');
+        const post_id = res.body._id;
+        
+        const update = await req(app).put("/posts/"+post_id).set("Authorization",token)
+        .attach('photo', 'public/uploads/images/test2.jpg');
+
+        expect(update.statusCode).toBe(200);
+        expect(update.body).toHaveProperty("title");
+        done();
     });
 
     it('should delete an existing post', async done => {
@@ -94,14 +121,12 @@ describe('post', () => {
         
         const update = await req(app).delete("/posts/"+post_id).set("Authorization",token);
         
-        done();
         expect(update.statusCode).toBe(200);
         expect(update.body).toHaveProperty("title", "New Title");
+        done();
     });
 
     it('should show  an specific post', async done => {
-        //Se podria crear con mongoose directamente, pero preferimos con 
-        //este metodo para que quede con el owner de una vez
         const res = await req(app).post("/posts").set("Authorization",token)
         .field('title', "New Title 1")
         .field('description', "New description")
@@ -110,9 +135,63 @@ describe('post', () => {
         
         const update = await req(app).get("/posts/"+post_id).set("Authorization",token);
         
-        done();
         expect(update.statusCode).toBe(200);
         expect(update.body).toHaveProperty("title", "New Title 1");
+        done();
+    });
+
+
+    it('should show all posts of a subcategory', async done => {
+
+        const res = await req(app).post("/posts").set("Authorization",token)
+        .field('title', "Title")
+        .field('description', "New description")
+        .field("category", "photography")
+        .field("subcategory", "wedding")
+        .attach('photo', 'public/uploads/images/test.jpg');
+        const post_id = res.body._id;
+        
+        const update = await req(app).get("/posts/subcategory/wedding").set("Authorization",token);
+        
+        expect(update.statusCode).toBe(200);
+        expect(update.body[0]).toHaveProperty("subcategory", "wedding");
+        expect(update.body).toHaveLength(1);
+        done();
+    });
+
+    it('should show all posts of a category', async done => {
+
+        const res = await req(app).post("/posts").set("Authorization",token)
+        .field('title', "Title")
+        .field('description', "New description")
+        .field("category", "photography")
+        .field("subcategory", "wedding")
+        .attach('photo', 'public/uploads/images/test.jpg');
+        const post_id = res.body._id;
+        
+        const update = await req(app).get("/posts/category/photography").set("Authorization",token);
+        
+        expect(update.statusCode).toBe(200);
+        expect(update.body[0]).toHaveProperty("category", "photography");
+        expect(update.body).toHaveLength(1);
+        done();
+    });
+
+    it('should show posts of a category/subcategory', async done => {
+
+        const res = await req(app).post("/posts").set("Authorization",token)
+        .field('title', "Title")
+        .field('description', "New description")
+        .field("category", "photography")
+        .field("subcategory", "wedding")
+        .attach('photo', 'public/uploads/images/test.jpg');
+        const post_id = res.body._id;
+        
+        const update = await req(app).get("/posts/photography/wedding").set("Authorization",token);
+        
+        expect(update.statusCode).toBe(200);
+        expect(update.body[0]).toHaveProperty("category", "photography");
+        done();
     });
 
 });
--- src/routes/user.test.js
+++ src/routes/user.test.js
@@ -6,14 +6,16 @@ const bcrypt = require('bcryptjs');
 
 describe.only('user', () => {
 
-    beforeEach(async () => {
+    beforeEach(async done => {
         for (let collection  in mongoose.connection.collections){
          await mongoose.connection.collections[collection].deleteMany({});   
         }
+        done();
     });
 
-    afterAll(async () => {
+    afterAll(async done => {
         await mongoose.disconnect();
+        done();
     });
 
     it('should signup a new user ', async done => {
@@ -22,10 +24,8 @@ describe.only('user', () => {
                     username:"jhhernan", name:"Jairo", lastname:"Hernandez"};
 
         const res = await req(app).post("/users/signup").send(user);
-        done();
-
- 
         expect(res.statusCode).toBe(200);
+        done();
         
     });
 
@@ -35,9 +35,9 @@ describe.only('user', () => {
                     name:"Jairo", lastname:"Hernandez"};
 
         const res = await req(app).post("/users/signup").send(user);
-        done();
         expect(res.statusCode).toBe(401);
         expect(res.body.message).toMatch("User validation failed: username: Username is required");
+        done();
      });
 
      it('shouldnt signup a new user if name is missing', async done => {
@@ -46,9 +46,9 @@ describe.only('user', () => {
                     username:"jhhernan", lastname:"Hernandez"};
 
         const res = await req(app).post("/users/signup").send(user);
-        done();
         expect(res.statusCode).toBe(401);
         expect(res.body.message).toMatch("User validation failed: name: Name is required");
+        done();
      });
 
 
@@ -61,10 +61,10 @@ describe.only('user', () => {
             email:"jairo@test.com",
             password:"123456", username:"jh", name: "Pedro", lastname:"Perez",
         });
-        done();
-
+        
         expect(res.statusCode).toBe(401);
         expect(res.body.message).toMatch("User validation failed: email: Email already exists");
+        done();
     })
 
     it('shouldnt sign in a user if email is wrong', async done => {
@@ -76,10 +76,10 @@ describe.only('user', () => {
             email:"jairo111@test.com",
             password:"123456"
         });
-        done();
-
+        
         expect(res.statusCode).toBe(401);
         expect(res.body).toHaveProperty("message","Wrong user/password");
+        done();
     })
 
     it('shouldnt sign in a user if password is wrong', async done => {
@@ -91,10 +91,10 @@ describe.only('user', () => {
             email:"jairo@test.com",
             password:"123456"
         });
-        done();
-
+        
         expect(res.statusCode).toBe(401);
         expect(res.body).toMatch("Wrong user/password");
+        done();
     })
 
   
@@ -107,13 +107,13 @@ describe.only('user', () => {
             email:"jairo@test.com",
             password:"12345"
         });
-        done();
-
+        
         expect(res.statusCode).toBe(200);
-        //expect(res.body).toHaveProperty("token");
+        done();
+        
     });
 
-     fit('should list all users in db', async done => {
+    it('should list all users in db', async done => {
           const user = {email:"jairo1@test.com", 
                       password: "12345",
                       username:"jhhernan", name:"Jairo", lastname:"Hernandez"};
@@ -131,5 +131,56 @@ describe.only('user', () => {
          done();
     });    
   
+
+    it('should update an existing user', async done => {
+        const user = {
+            email: "jairo1@test.com",
+            password: "12345",
+            username: "jhhernan", name: "Jairo", lastname: "Hernandez"
+        };
+
+        const auth = await req(app).post("/users/signup").send(user);
+        const token = auth.body;
+
+        const res = await req(app).put("/users").set("Authorization", token).send({ username: "jh" });
+
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toHaveProperty("username", "jh");
+        done();
+    });   
+
+    it('should delete an existing user', async done => {
+        const user = {
+            email: "jairo1@test.com",
+            password: "12345",
+            username: "jhhernan", name: "Jairo", lastname: "Hernandez"
+        };
+
+        const auth = await req(app).post("/users/signup").send(user);
+        const token = auth.body;
+
+        const res = await req(app).delete("/users").set("Authorization", token);
+
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toHaveProperty("email", "jairo1@test.com");
+        done();
+    });   
+
+    it('should show the details of the user', async done => {
+        const user = {
+            email: "jairo1@test.com",
+            password: "12345",
+            username: "jhhernan", name: "Jairo", lastname: "Hernandez"
+        };
+
+        const auth = await req(app).post("/users/signup").send(user);
+        const token = auth.body;
+
+        const res = await req(app).get("/users/get").set("Authorization", token);
+
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toHaveProperty("email", "jairo1@test.com");
+        done();
+    });   
 
 });
