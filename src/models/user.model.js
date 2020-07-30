5a8277a1696a0b12bb5735f232fef19cc6851aa9
Diego <danoguera@gmail.com>
2020-07-30 15:58:55 -0500

    User model last update

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,5 @@
 const { Schema, model, models} = require('mongoose');
 
-
 const emailTest = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 const uniquemail =  {
